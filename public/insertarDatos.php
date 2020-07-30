6fbeee2b95a5cd9b8c8b83d42ac456adb30b951b
Oscar García <oscar.github@linaresdigital.com>
2020-07-30 10:03:51 +0000

    Comprobaciones finales

--- public/insertarDatos.php
+++ public/insertarDatos.php
@@ -10,12 +10,26 @@ if (mysqli_connect_error() !== NULL) {
     die('Error de Conexión: ' . htmlspecialchars(mysqli_connect_error()));
 }
 
+/* Es importante repetir las validaciones en el lado del servidor */
+$nombre = trim($_POST['nombre']);
+if ($nombre == '' || strlen($nombre) > 30) {
+    die('Nombre inválido, verificar campo');
+}
+$apellido = trim($_POST['apellido']);
+if ($apellido == '' || strlen($apellido) > 30) {
+    die('Apellido inválido, verificar campo');
+}
+$email = trim($_POST['email']);
+if ($email == '') {
+    die('Email inválido, verificar campo');
+}
+
 /* INDISPENSABLE escapar las cadenas con "mysqli_real_escape_string" */
-$nombre = mysqli_real_escape_string($conn, $_POST['nombre']);
-$apellido = mysqli_real_escape_string($conn, $_POST['apellido']);
+$nombre = mysqli_real_escape_string($conn, $nombre);
+$apellido = mysqli_real_escape_string($conn, $apellido);
 $curp = mysqli_real_escape_string($conn, $_POST['curp']);
 //$fecha_enfermedad = mysqli_real_escape_string($conn, $_POST['fecha_enfermedad']);
-$email = mysqli_real_escape_string($conn, $_POST['email']);
+$email = mysqli_real_escape_string($conn, $email);
 
 /* Deberías usar en su lugar la función SQL "NOW()" */
 //$time = mysqli_real_escape_string($conn, date("g:i a"));
